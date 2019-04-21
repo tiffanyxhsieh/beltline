2dc750f6521644dc97ce611b6ae2eff25a712d32
Wei Wu <weiwu@Weis-MacBook-Pro.local>
2019-04-20 22:04:13 -0400

    screen 20 only need to update

---
+++ src/main/java/com/group51/beltline/controllers/screencontrollers/Screen19Controller.java
@@ -0,0 +1,57 @@
+package com.group51.beltline.controllers.screencontrollers;
+
+import com.group51.beltline.models.*;
+import com.group51.beltline.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class Screen19Controller {
+    @Autowired
+    private SiteRepository siteRepository;
+
+    //Get ALL Site
+    @GetMapping(path = "/sites")
+    public @ResponseBody
+    Collection<Site> getSites(){
+        return siteRepository.getAllSites();
+    }
+
+    //Get site by name
+    @GetMapping(path = "/site") // Map ONLY GET Requests
+    public @ResponseBody
+    Site getSiteByName(@Param(value="name") String name){
+        return siteRepository.getSiteByName(name);
+    }
+
+    //create a site
+    @Transactional
+    @PostMapping(path="/site")
+    public @ResponseBody
+    int createNewsite(@Param("Name") String name, @Param("Zipcode") String zipcode, @Param("Address") String address, @Param("OpenEveryday") String openeveryday, @Param("Manager") String manager){
+        return siteRepository.createNewSite(name, zipcode, address, openeveryday, manager);
+    }
+
+    //update a site
+    @Transactional
+    @PutMapping(path="/site")
+    public @ResponseBody
+    int putNewsite(@Param("Name") String name, @Param("Zipcode") String zipcode, @Param("Address") String address, @Param("OpenEveryday") String openeveryday, @Param("Manager") String manager){
+        return siteRepository.updateNewSite(name, zipcode, address, openeveryday, manager);
+    }
+
+    //delete a site
+    @Transactional
+    @DeleteMapping(path="/site")
+    public @ResponseBody
+    int deleteSite(@Param("Name") String name){
+        return siteRepository.deleteSite(name);
+    }
+}
