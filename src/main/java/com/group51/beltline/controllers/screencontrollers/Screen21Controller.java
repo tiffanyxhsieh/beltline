e05d162e2f70f6b084c246bb29e3a5f9a163b0dd
Wei Wu <weiwu@Weis-MacBook-Pro.local>
2019-04-20 22:23:10 -0400

    screen 21 just need to create site

---
+++ src/main/java/com/group51/beltline/controllers/screencontrollers/Screen21Controller.java
@@ -0,0 +1,27 @@
+package com.group51.beltline.controllers.screencontrollers;
+
+import com.group51.beltline.models.*;
+import com.group51.beltline.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class Screen21Controller {
+    @Autowired
+    private SiteRepository siteRepository;
+
+    //create a site
+    @Transactional
+    @PostMapping(path="/screen21/create")
+    public @ResponseBody
+    int createNewsite(@Param("Name") String name, @Param("Zipcode") String zipcode, @Param("Address") String address, @Param("OpenEveryday") String openeveryday, @Param("Manager") String manager){
+        return siteRepository.createNewSite(name, zipcode, address, openeveryday, manager);
+    }
+}
