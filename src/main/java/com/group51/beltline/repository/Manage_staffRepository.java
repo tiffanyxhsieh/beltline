559f19dfdc05de73d1f8c69b5b1c16e6bb671280
Wei Wu <weiwu@lawn-143-215-126-87.lawn.gatech.edu>
2019-04-23 10:10:11 -0400

    allow partial serach for 28

--- src/main/java/com/group51/beltline/repository/Manage_staffRepository.java
+++ src/main/java/com/group51/beltline/repository/Manage_staffRepository.java
@@ -21,7 +21,7 @@ public interface Manage_staffRepository extends JpaRepository<Manage_staff, Stri
     // and a.username in (select username from shift_sites where sitename = :sitename)
     // group by a.username"
 
-    @Query(value = "select concat(u.firstname,' ',u.lastname) as staff_name,count(a.name) as num_event_shifts from assign_to as a join event as e on e.name = a.name and e.startdate = a.startdate and e.sitename = a.sitename join user as u on a.username =u.username where (a.startdate between :startdate and :enddate or e.enddate between :startdate and :enddate) and (:firstname is null or u.firstname=:firstname) and (:lastname is null or u.lastname=:lastname) and a.username in (select username from assign_to where (:sitename is null or sitename = :sitename)) group by a.username", nativeQuery = true)
+    @Query(value = "select concat(u.firstname,' ',u.lastname) as staff_name,count(a.name) as num_event_shifts from assign_to as a join event as e on e.name = a.name and e.startdate = a.startdate and e.sitename = a.sitename join user as u on a.username =u.username where (a.startdate between :startdate and :enddate or e.enddate between :startdate and :enddate) and (:firstname is null or u.firstname like CONCAT('%', :firstname, '%')) and (:lastname is null or u.lastname like CONCAT('%', :lastname, '%')) and a.username in (select username from assign_to where (:sitename is null or sitename like CONCAT('%', :sitename, '%'))) group by a.username", nativeQuery = true)
     Collection<Manage_staff> filter(@Param("startdate") String startdate, 
     @Param("enddate") String enddate,
     @Param("sitename") String sitename, @Param("firstname") String firstname, @Param("lastname") String lastname);
